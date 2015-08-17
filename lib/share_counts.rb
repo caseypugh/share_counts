bb676eabfc6ac7d3bf5c80e20eaed4cc1f87ff70
Casey Pugh <me@caseypugh.com>
2015-08-17 15:22:34 -0400

    upgrade to use Facebook FQL API. Disabled digg, reddit, stumbleupon since they are broken

--- lib/share_counts.rb
+++ lib/share_counts.rb
@@ -11,12 +11,12 @@ module ShareCounts
   end
 
   def self.supported_networks
-    %w(reddit digg twitter facebook linkedin stumbleupon googlebuzz)
+    %w(twitter facebook linkedin stumbleupon tumblr)
   end
   
   def self.reddit url, raise_exceptions = false
     try("reddit", url, raise_exceptions) {
-      extract_count from_json( "http://www.reddit.com/api/info.json", :url => url ), 
+      extract_count from_json("http://www.reddit.com/api/info.json", :url => url ), 
         :selector => "data/children/data/score" 
     }
   end
@@ -27,29 +27,30 @@ module ShareCounts
   
   def self.digg url, raise_exceptions = false
     try("digg", url, raise_exceptions) {
-      extract_count from_json( "http://services.digg.com/2.0/story.getInfo", :links => url ), 
+      extract_count from_json("http://services.digg.com/2.0/story.getInfo", :links => url ), 
         :selector => "stories/diggs"
     }
   end
 
   def self.twitter url, raise_exceptions = false
     try("twitter", url, raise_exceptions) {
-      extract_count from_json( "http://urls.api.twitter.com/1/urls/count.json", :url => url), 
+      extract_count from_json("http://urls.api.twitter.com/1/urls/count.json", :url => url), 
         :selector => "count"
     }
   end
 
+  # https://graph.facebook.com/fql?q=SELECT%20like_count,%20total_count,%20share_count,%20click_count,%20comment_count%20FROM%20link_stat%20WHERE%20url%20=%20%22http://sevenly.org/FightForHer%22
   def self.facebook url, raise_exceptions = false
     try("facebook", url, raise_exceptions) {
-      extract_count from_json("http://api.facebook.com/restserver.php", :v => "1.0", :method => "links.getStats",  
-       :urls => url, :callback => "fb_sharepro_render", :format => "json" ), :selector => "total_count"
+      extract_count from_json("https://graph.facebook.com/fql", :q => %{SELECT share_count FROM link_stat WHERE url="#{url}"}), 
+      :selector => "data/share_count"
     }
   end
 
   def self.linkedin url, raise_exceptions = false 
     try("linkedin", url, raise_exceptions) {
-      extract_count from_json("http://www.linkedin.com/cws/share-count", 
-        :url => url, :callback => "IN.Tags.Share.handleCount" ), :selector => "count"
+      extract_count from_json("http://www.linkedin.com/countserv/count/share", 
+        :url => url, :format => 'json'), :selector => "count"
     }
   end
 
@@ -69,7 +70,7 @@ module ShareCounts
   end
 
   def self.tumblr url, raise_exceptions = false
-    try("twitter", url, raise_exceptions) {
+    try("tumblr", url, raise_exceptions) {
       extract_count from_json("http://api.tumblr.com/v2/share/stats", :url => url), 
         :selector => "response/note_count"
     }
@@ -83,6 +84,4 @@ module ShareCounts
     selections.map{|name| name.downcase}.select{|name| supported_networks.include? name.to_s}.inject({}) {
        |r, c| r[c.to_sym] = ShareCounts.send(c, url); r }
   end
-  
 end
-
